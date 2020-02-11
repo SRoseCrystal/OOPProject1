babb245346a26c4d190e48c22ec7cac8756993d4
Silvana <silvanarosecrystal@gmail.com>
2020-02-11 14:16:18 +0000

    E

---
+++ Countries/CountryHandler/CountryHandler/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace CountryHandler
+{
+    class CountryImport
+    {
+        int main()
+        {
+            File* fp;
+            char str[MAXCHAR];
+            char* filename = "c:\\countries.txt";
+
+            fp = fopen(filename, "r");
+            if (fp == NULL)
+            {
+                printf("Could not open file %s", filename);
+                return 1;
+            }
+            while (fgets(str, MAXCHAR, fp) != NULL)
+                printf("%s", str);
+            fclose(fp);
+            return 0;
+        }
+    }
+}
