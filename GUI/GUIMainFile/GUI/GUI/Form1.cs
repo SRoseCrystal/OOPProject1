36d0bc16b15337168d4fbacd018da2838b98f268
SRoseCrystal <SRoseCrystal@users.noreply.github.com>
2020-02-22 20:14:45 +0000

    Nearly complete
    finished system to add countries from a json file and add buttons for each country.
    waiting on others to complete final bits.

---
+++ GUI/GUIMainFile/GUI/GUI/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.ID;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace GUI
+{
+    public partial class Form1 : Form
+    {
+        Countries countries = new Countries();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private DataTable LoadJson()
+        {
+            using (StreamReader file = new StreamReader("..."))
+            {
+                using jsonFile = file.ReadToEnd();
+                DataSet countryList = JsonConvert.DeserializeObject<DataSet>(jsonFile);
+                DataTable countryTable = countryList.Tables("Countries");
+                return countryTable;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Text = "EU Council Voting Calculator";
+            int countryNumber = 28;
+            int nameandvote = 2;
+            int countrynumber = 0;
+            DataTable file = LoadJson();
+            InitTable(tableLayoutPanel1, countryNumber, nameandvote);
+            foreach(DataRow dataRow in file.Rows)
+            {
+                string country = dataflow["name"].ToString();
+                if(countrynumber < countryNumber)
+                {
+                    LoadName(country, 0, countrynumber);
+                    LoadButtons(country, 1, countrynumber);
+                    countrynumber++;
+                }
+            }
+        }
+    }
+}
