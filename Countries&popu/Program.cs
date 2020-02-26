56e52af835c4f3e6fefcfbe5c22cd920174d5be9
ZoeSobowale <38224967+ZoeSobowale@users.noreply.github.com>
2020-02-26 14:26:44 +0000

    Add files via upload

---
+++ Countries&popu/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Assigment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] countries = { "Austria", "Belgium", "Bulgaria", "Croatia", "Cyprus", "Czech Republic", "Denmark", "Estonia", "Finland", "France", "Germany", "Greece", "Hungary", "Ireland", "Italy", "Latvia", "Lithuania", "Luxembourg", "Malta", "Netherlands", "Poland", "Portugal", "Romania", "Slovakia", "Slovenia", "Spain", "Sweden" };
+            double[] population = { 1.98, 2.56, 1.56, 0.91, 0.20, 2.35, 1.30, 0.30, 1.23, 14.98, 18.54, 2.40, 2.18, 1.10, 13.65, 0.43, 0.62, 0.14, 0.11, 3.89, 8.49, 2.30, 4.34, 1.22, 0.47, 10.49, 2.29 };
+            // This stores the countries and populations for ecah country 
+            // The population exactly correlate with the countries 
+            
+        }
+    }
+}
